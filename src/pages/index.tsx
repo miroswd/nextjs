21790ff8be642eb0883f8f4ed447ee06e7a3eb8b
miroswd <altamirsantos93@hotmail.com>
2023-07-17 14:23:44 -0300

    feat: client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react"
 import { Title } from "../styles/pages/Home"
 
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 
 export default function Home() {
+
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  // when it doesn't need to be indexed for search engines
+  useEffect(() => {
+    fetch("http://localhost:3333/recommended").then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data)
+      })
+    })
+  }, [])
+
   return (
     <div>
-      <Title>Hello World</Title>
+      <Title>Products</Title>
+
+      {recommendedProducts.map(recommendedProduct => (
+        <li key={recommendedProduct.id}>
+          {recommendedProduct.title}
+        </li>
+      ))}
     </div>
   )
 }
