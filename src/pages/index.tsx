e92c54f286587e1552b0b1ee642f67ce269b41e3
miroswd <altamirsantos93@hotmail.com>
2023-07-17 14:04:56 -0300

    feat: css generated with server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Title } from "../styles/pages/Home"
+
+
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   )
 }
