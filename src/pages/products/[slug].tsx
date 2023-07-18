f529962b48e59bfdc8d05a7b9028023c0f17ccd7
miroswd <altamirsantos93@hotmail.com>
2023-07-17 22:42:48 -0300

    feat: dynamic import to components

---
+++ src/pages/products/[slug].tsx
@@ -0,0 +1,37 @@
+// import AddToCartModel from "@/src/components/AddToCartModel";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+
+const AddToCartModel = dynamic(
+  () => import("@/src/components/AddToCartModel"), 
+  { loading: () => <p>Carregando...</p> }
+);
+
+
+export default function Product(){
+
+  const router = useRouter();
+
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true)
+  }
+
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>
+        Add to cart
+      </button>
+
+      {
+        isAddToCartModalVisible && <AddToCartModel />
+      }
+    </div>
+  );
+}
